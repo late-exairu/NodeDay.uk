3752963a38fdda6d5763ec6d48deda7a7504c952
Robert Haritonov <r@rhr.me>
2020-02-26 17:17:44 +0000

    configure deploy

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -7,19 +7,40 @@ const args = minimist(process.argv.slice(2));
 gulp.task('deploy', function() {
   const remotePath = '/';
   const conn = ftp.create({
-    /* TODO: set deploying credentials */
-    // host: 'buff.elastictech.org',
-    // user: args.user,
-    // password: args.password,
+    host: 'buff.elastictech.org',
+    user: args.user,
+    password: args.password,
     log: gutil.log,
     parallel: 10,
   });
 
-  gulp.src(['./build/**/*.*']).pipe(conn.dest(remotePath));
+  // Always deploy HTML
+  gulp.src([
+    './build/*.*'
+  ])
+    .pipe(conn.dest(remotePath));
 
-  gulp
-    .src(['./sponsors/**/*.*', '!./sponsors/node_modules/**/*.*'])
-    .pipe(conn.dest(`${remotePath}/sponsors`));
+  // Always deploy CSS
+  gulp.src([
+    './build/css/**/*.*'
+  ])
+    .pipe(conn.dest('/css'));
+
+  // Always deploy JS
+  gulp.src([
+    './build/js/**/*.*'
+  ])
+    .pipe(conn.dest('/js'));
+
+  // Compare size of other files before deploy
+  gulp.src([
+    './build/**/*.*',
+    '!./build/*.*',
+    '!./build/css/**/*.*',
+    '!./build/js/**/*.*'
+  ])
+    .pipe(conn.differentSize(remotePath))
+    .pipe(conn.dest(remotePath));
 
   // uncomment to deploy last year versions
   // gulp.src([
